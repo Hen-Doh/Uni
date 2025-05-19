4827263d4259442d5782c820c9f398df9e82d05a
HenDohr <hendoh@gmx.de>
2025-05-19 17:14:44 +0200

    Info2 Uebung04 fertig

--- Info2/Uebungen/Uebung04/Parser.hs
+++ Info2/Uebungen/Uebung04/Parser.hs
@@ -1,26 +1,3 @@
-{-
-ll_parser1 :: String -> Bool
-ll_parser1 a = ll_parse a "id_list" 
-    where 
-    ll_parse :: String -> String -> Bool
-    ll_parse (';':'$':['$']) "id_list_tail" = True
-    ll_parse (',':'i':'d':xs) "id_list_tail" = ll_parse xs "id_list_tail"
-    ll_parse ('i':'d':xs) "id_list" = ll_parse xs "id_list_tail"
-    ll_parse _ _ = False 
-
-match :: String -> [String] -> [String]
-match t (x:xs) = id_list satz
-match a satz = error (show satz) --(concat x ++" is not a valid input for" ++ a)
-
-id_list_tail :: [ String ] -> [ String ]
-id_list_tail (";":"$$":[]) =  []
-id_list_tail (",":"id":xs) = match "id_list_tail" xs
-id_list_tail x = error (concat x ++" is not a valid input for id_list_tail")
-
-id_list :: [ String ] -> [ String ]
-id_list ("id":xs) = match "id_list_tail" xs
-id_list x = error (show x++ "is not a valid input for id_list")
--}
 
 match1 :: String -> [String] -> [String]
 match1 t [] = error ("[] is not matching the expected "++ t)
@@ -39,7 +16,12 @@ id_list :: [ String ] -> [ String ]
 id_list satz = id_list_tail $ match1 "id" satz
 
 --id_list [ "id" , "," , "id" , ";" , "$$" ]
+--id_list [ "id" , "," , "id" , ";" , "$" ]
+test1 :: [String]
+test1 = id_list [ "id" , "," , "id" , ";" , "$$" ]
 
+test2 :: [String]
+test2 = id_list [ "id" , "," , "id" , ";" , "$" ]
 
 -------------------------------------------2. Parser--------------------------------------------------------
 
@@ -59,29 +41,38 @@ prog satz = match '$' $ expr $ Just satz    -- "$"" wird als letztes überprüft
 
 expr :: Maybe String -> Maybe String
 expr Nothing = Nothing
-expr satz = ttail $ term satz  
+expr satz = ttail $ term satz
 
 term :: Maybe String -> Maybe String
 term Nothing = Nothing
 term satz = ftail $ factor satz
 
-ttail :: Maybe String -> Maybe String
-ttail Nothing = Nothing
-ttail (Just []) = Just []
-ttail (Just [x]) = Just [x]
-ttail (Just (x:xs))
-    | x== '+' = ttail $ term $ Just xs
-    | otherwise = Just (x:xs)
-
-
 factor :: Maybe String -> Maybe String
 factor Nothing = Nothing
 factor satz = match 'c' satz
 
+ttail :: Maybe String -> Maybe String
+ttail Nothing = Nothing
+ttail (Just (x:xs))
+    | x== '+' = ttail $ term $ Just xs
+    | otherwise = Just (x:xs)
+ttail _ = Nothing -- how to check for empty string/single item string?
+
 ftail :: Maybe String -> Maybe String
 ftail Nothing = Nothing
-ftail (Just []) = Just []
-ftail (Just [x]) = Just [x]
 ftail (Just (x:xs))
-    | x== '+' = ftail $ term $ match '+' $  Just ([x]++xs)
+    | x== '*' = ftail $ term $ match '*' $  Just ([x]++xs)
     | otherwise =  Just([x] ++ xs)
+ftail _ = Nothing
+
+test3:: Maybe String
+test3 = prog "c+c*c$"
+
+test4:: Maybe String
+test4 = prog "c+cc$"
+
+test5:: Maybe String
+test5 = prog "c+*c"
+
+test6:: Maybe String
+test6 = prog "*c$"
